eb6038b4c0b7601c1352a43b90207a83c247cce6
Vitalik Melnik <vitalikmelnik20000@gmail.com>
2018-12-22 19:08:37 +0200

    gql add , config router

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
-import Koa from 'koa';
-
 
 import { port } from './utils/config';
-
-const app = new Koa();
-
-app.use(async ctx => {
-    ctx.body = 'Hello world';
-});
-
-
+import app from './app';
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
-
-
-
